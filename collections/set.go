41c375cfcc1090aaefec6a28b192cf8a991fde33
Allan <allan.borba@prevision.com.br>
2025-09-07 11:24:55 -0300

    refactor: extract Set.add method to improve code reusability

--- collections/set.go
+++ collections/set.go
@@ -6,10 +6,14 @@ type Set[T comparable] struct {
 
 func NewSet[T comparable](elements []T) *Set[T] {
 	set := &Set[T]{make(map[T]struct{})}
-	for _, element := range elements {
-		set.elemements[element] = struct{}{}
+	for _, el := range elements {
+		set.add(el)
 	}
 	return set
+}
+
+func (this *Set[T]) add(el T) {
+	this.elemements[el] = struct{}{}
 }
 
 func (this *Set[T]) Len() int {
