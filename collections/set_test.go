a95554f669991a72a0717a0f4a3e2faa72846ddc
Allan <allan.borba@prevision.com.br>
2025-09-07 11:31:59 -0300

    refactor: consolidate set initialization tests using helper assertion function

--- collections/set_test.go
+++ collections/set_test.go
@@ -6,26 +6,14 @@ import (
 	"github.com/allanborba/utilitaries/collections"
 )
 
-func TestIinitializeEmptySet(t *testing.T) {
-	set := collections.NewSet([]int{})
-
-	if set.Len() != 0 {
-		t.Errorf("expected 0, got %d", set.Len())
-	}
+func TestIinitializeSetWithCorrectlyLength(t *testing.T) {
+	assertSetLength(t, 0, collections.NewSet([]int{}).Len())
+	assertSetLength(t, 3, collections.NewSet([]int{1, 2, 3}).Len())
+	assertSetLength(t, 2, collections.NewSet([]string{"1", "2"}).Len())
 }
 
-func TestIinitializeSetOfIntegers(t *testing.T) {
-	set := collections.NewSet([]int{1, 2, 3})
-
-	if set.Len() != 3 {
-		t.Errorf("expected 0, got %d", set.Len())
-	}
-}
-
-func TestInitializeSetOfStrings(t *testing.T) {
-	set := collections.NewSet([]string{"1", "2", "3", "4"})
-
-	if set.Len() != 4 {
-		t.Errorf("expected 0, got %d", set.Len())
+func assertSetLength(t *testing.T, result int, expected int) {
+	if result != expected {
+		t.Errorf("expected %d, got %d", expected, result)
 	}
 }
