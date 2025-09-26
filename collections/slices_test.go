fa5c59852a34ff6fdfa2f6984bc97c220905065f
Allan <allan.borba@prevision.com.br>
2025-09-26 13:34:13 -0300

    test: add struct type support to TestSlicesContains test cases

--- collections/slices_test.go
+++ collections/slices_test.go
@@ -8,10 +8,25 @@ import (
 )
 
 func TestSlicesContains(t *testing.T) {
-	slice1 := []int{1, 2, 3}
+	type TestStruct struct {
+		A int
+		b string
+	}
 
-	asserts.Equal(t, true, collections.Contains(slice1, 1))
-	asserts.Equal(t, true, collections.Contains(slice1, 2))
-	asserts.Equal(t, true, collections.Contains(slice1, 3))
-	asserts.Equal(t, false, collections.Contains(slice1, 4))
+	sliceOfInt := []int{1, 2, 3}
+	sliceOfStruct := []TestStruct{
+		{A: 1, b: "a"},
+		{A: 2, b: "b"},
+		{A: 3, b: "c"},
+	}
+
+	asserts.Equal(t, true, collections.Contains(sliceOfInt, 1))
+	asserts.Equal(t, true, collections.Contains(sliceOfInt, 2))
+	asserts.Equal(t, true, collections.Contains(sliceOfInt, 3))
+	asserts.Equal(t, false, collections.Contains(sliceOfInt, 4))
+
+	asserts.Equal(t, true, collections.Contains(sliceOfStruct, TestStruct{A: 1, b: "a"}))
+	asserts.Equal(t, true, collections.Contains(sliceOfStruct, TestStruct{A: 2, b: "b"}))
+	asserts.Equal(t, true, collections.Contains(sliceOfStruct, TestStruct{A: 3, b: "c"}))
+	asserts.Equal(t, false, collections.Contains(sliceOfStruct, TestStruct{A: 4, b: "d"}))
 }
