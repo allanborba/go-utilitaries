ca2a32e336efde6f63d4a3b1da290c6e827ddc7f
Allan <allan.borba@prevision.com.br>
2025-10-11 17:45:54 -0300

    feat: add JSON encoding/decoding utilities for HTTP and IO operations

--- json_utils/helper.go
+++ json_utils/helper.go
@@ -1,6 +1,14 @@
 package jsonutils
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func ToJsonString(value interface{}) string {
+	return string(ToJsonBytes(value))
+}
 
 func ToJsonBytes(value interface{}) []byte {
 	json, err := json.Marshal(value)
@@ -8,4 +16,21 @@ func ToJsonBytes(value interface{}) []byte {
 		panic(err)
 	}
 	return json
+}
+
+func DecodeIoReader[T any](ioReader io.ReadCloser) *T {
+	var input T
+	err := json.NewDecoder(ioReader).Decode(&input)
+	if err != nil {
+		panic(err)
+	}
+
+	return &input
+}
+
+func EncodeJson[T any](w http.ResponseWriter, toEncode T) {
+	err := json.NewEncoder(w).Encode(toEncode)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
